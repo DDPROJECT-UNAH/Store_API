7a3bec39bb7abef2e2187914e2868a104ae05b32
Jeyson Mejia <56891343+SamJ17@users.noreply.github.com>
2024-08-19 22:23:06 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const config = require('../config.js');
-const auth = require('./user-auth/route');
-const  {verifyToken} = require('../src/middleware/authMiddleware.js')
-const {routerProductos} = require('./routes/producto');
-
+const express = require("express");
+const dotenv = require("dotenv");
+const config = require("../config.js");
+const auth = require("./user-auth/route");
+const { verifyToken } = require("../src/middleware/authMiddleware.js");
+const { routerProductos } = require("./routes/producto");
+const cors = require("cors");
 const app = express();
+app.disable("x-powered-by"); // deshabilitar la cabecera x-powered-by
 app.use(express.json());
+app.use(cors());
+
 dotenv.config();
 const PORT = config.app.port;
 
-app.use('/auth', auth);
+app.use("/auth", auth);
 app.all("*", (req, res, next) => {
-    verifyToken(req, res, next);
+  verifyToken(req, res, next);
+});
+app.use("/productos", routerProductos);
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: true,
+    message: "La ruta no existe",
+  });
 });
 
-app.use('/productos',routerProductos);
-
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
